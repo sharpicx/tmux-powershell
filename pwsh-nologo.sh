06df66340e5b578e25f2dff12f16854eee18cde1
sharpicx <44630640+sharpicx@users.noreply.github.com>
2025-04-09 02:10:25 +0700

    Update pwsh-nologo.sh

--- pwsh-nologo.sh
+++ pwsh-nologo.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
-fallback_win_path="C:\\Users\\yourwindowsusername"
-win_path=$(cat /mnt/c/Users/yourwindowsusername/.wsl_pwd 2>/dev/null)
-if [[ "$win_path" =~ ^[A-Z]:\\ ]]; then
+fallback_win_path="C:\\Users\\via"
+win_path=$(tr -d '\r\n' < /mnt/c/Users/via/.wsl_pwd)
+if [[ "$win_path" =~ ^[A-Z]:\\\\ ]]; then
     final_path="$win_path"
 else
     final_path="$fallback_win_path"
 fi
 final_path=$(echo "$final_path" | sed 's/\\\\/\\/g')
-exec pwsh.exe -noexit -Command "cd $final_path"
+exec pwsh.exe -noexit -Command "Set-Location -Path \"$final_path\""
