80dcd2025162db6dcdf2eb2e938d5a39dfd9200c
sharpicx <44630640+sharpicx@users.noreply.github.com>
2025-04-09 01:45:26 +0700

    Create profile.ps1

---
+++ profile.ps1
@@ -0,0 +1,23 @@
+function Update-WSLPath {
+    $pwdPath = (Get-Location).Path
+    $escapedPath = $pwdPath.Replace('\', '\\')
+    $outFile = "$env:USERPROFILE\.wsl_pwd"
+    try {
+        $escapedPath | Out-File -Encoding ASCII -Force -FilePath $outFile
+    } catch {
+        Write-Warning "Failed to write to $outFile"
+    }
+}
+Remove-Alias cd
+function cd {
+    param(
+        [string]$path = "$HOME"
+    )
+
+    if (Test-Path $path) {
+        Set-Location $path
+        Update-WSLPath
+    } else {
+        Write-Host "Path?: $path"
+    }
+}
